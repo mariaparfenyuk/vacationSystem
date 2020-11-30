d1f89e8bf59d28a72891917cc8cb0267aad76ff0
Maria Parfenyuk <Maria_Parfenyuk@epam.com>
2020-11-30 23:08:36 +0300

    feat: add configs

---
+++ src/config/selectors.js
@@ -0,0 +1,12 @@
+export const pageElements = {
+  title: "//span[text()='Vacations']",
+  addVacation: "//*[@id='add-new-vac']/i",
+}
+
+export const vacationPopUp = {
+  checkBox: "span.mdl-checkbox__ripple-container.mdl-js-ripple-effect.mdl-ripple--center",
+  fromDate: "//*[@id='from']",
+  endDate: "//*[@id='to']",
+  cancelButton: "//*[@id='cancel']",
+  SaveButton: "//*[@id='save']"
+}
---
+++ src/config/urlConfig.js
@@ -0,0 +1,5 @@
+const vacationSystemUrl = {
+  urlVacation: 'https://test.webtech.by/vac-system/'
+}
+
+export default vacationSystemUrl
